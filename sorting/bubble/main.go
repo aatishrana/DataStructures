61e15041e71731e8875bc5ec32a54f3a2c2cd78b
Aatish <aatishrana8@gmail.com>
2017-12-23 16:54:21 +0530

    bubble sort

---
+++ sorting/bubble/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	u "utils"
+)
+
+func main() {
+	a := u.RandomArray()
+	fmt.Println("Sorting", len(a), "items")
+	bubbleSort(a)
+}
+
+func bubbleSort(a []int) {
+	defer u.Elapsed("Bubble sort")()
+	for i := len(a) - 1; i > 1; i-- {
+		for j := 0; j < i; j++ {
+			if a[j] > a[j+1] {
+				u.Swap(&a[j], &a[j+1])
+			}
+		}
+	}
+}
---
+++ vendor/utils/utils.go
@@ -0,0 +1,34 @@
+package utils
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func RandomArray() []int {
+
+	l := 50000
+	m := 100000
+
+	a := make([]int, l, l)
+
+	for i := 0; i < l; i++ {
+		a[i] = rand.Intn(m)
+	}
+
+	return a
+}
+
+func Swap(a *int, b *int) {
+	temp := *a
+	*a = *b
+	*b = temp
+}
+
+func Elapsed(what string) func() {
+	start := time.Now()
+	return func() {
+		fmt.Printf("%s took %v\n", what, time.Since(start))
+	}
+}
